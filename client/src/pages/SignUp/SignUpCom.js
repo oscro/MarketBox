5580dfc6d84b6ba90d0dfd997e177a84d7a7663d
The Real One <gutierrez88@gmail.com>
2019-04-27 09:13:33 -0400

    signIn and signUp

---
+++ client/src/pages/SignUp/SignUpCom.js
@@ -0,0 +1,138 @@
+// import withRoot from './modules/withRoot';
+// --- Post bootstrap -----
+// import compose from 'recompose/compose';
+// import AppAppBar from '../../components/modules/views/AppAppBar';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+// import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { Field, Form, FormSpy } from 'react-final-form';
+import Typography from '../../components/modules/components/Typography';
+import AppFooter from '../../components/modules/views/AppFooter';
+import AppForm from '../../components/modules/views/AppForm';
+import { email, required } from '../../components/modules/form/validation';
+import RFTextField from '../../components/modules/form/RFTextField';
+import FormButton from '../../components/modules/form/FormButton';
+import FormFeedback from '../../components/modules/form/FormFeedback';
+
+const styles = theme => ({
+  form: {
+    marginTop: theme.spacing.unit * 6,
+  },
+  button: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  feedback: {
+    marginTop: theme.spacing.unit * 2,
+  },
+});
+
+class SignUpCom extends React.Component {
+  state = {
+    sent: false,
+  };
+
+  validate = values => {
+    const errors = required(['firstName', 'lastName', 'email', 'password'], values, this.props);
+
+    if (!errors.email) {
+      const emailError = email(values.email, values, this.props);
+      if (emailError) {
+        errors.email = email(values.email, values, this.props);
+      }
+    }
+
+    return errors;
+  };
+
+  handleSubmit = () => {};
+
+  render() {
+    const { classes } = this.props;
+    const { sent } = this.state;
+
+    return (
+      <React.Fragment>
+        
+        <AppForm>
+          <React.Fragment>
+            <Typography variant="h3" gutterBottom marked="center" align="center">
+              Sign Up
+            </Typography>
+            <Typography variant="body2" align="center">
+              <Link href="/signin" underline="always">
+                Already have an account?
+              </Link>
+            </Typography>
+          </React.Fragment>
+          <Form
+            onSubmit={this.handleSubmit}
+            subscription={{ submitting: true }}
+            validate={this.validate}
+          >
+            {({ handleSubmit, submitting }) => (
+              <form onSubmit={handleSubmit} className={classes.form} noValidate>
+                <Field
+                    autoFocus
+                    component={RFTextField}
+                    autoComplete="cName"
+                    fullWidth
+                    label="Comany Name"
+                    name="comName"
+                    required
+                />
+                <Field
+                  autoComplete="email"
+                  component={RFTextField}
+                  disabled={submitting || sent}
+                  fullWidth
+                  label="Email"
+                  margin="normal"
+                  name="email"
+                  required
+                />
+                <Field
+                  fullWidth
+                  component={RFTextField}
+                  disabled={submitting || sent}
+                  required
+                  name="password"
+                  autoComplete="current-password"
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                />
+                <FormSpy subscription={{ submitError: true }}>
+                  {({ submitError }) =>
+                    submitError ? (
+                      <FormFeedback className={classes.feedback} error>
+                        {submitError}
+                      </FormFeedback>
+                    ) : null
+                  }
+                </FormSpy>
+                <FormButton
+                  className={classes.button}
+                  disabled={submitting || sent}
+                  color="secondary"
+                  fullWidth
+                >
+                  {submitting || sent ? 'In progress…' : 'Sign Up'}
+                </FormButton>
+              </form>
+            )}
+          </Form>
+        </AppForm>
+        <AppFooter />
+      </React.Fragment>
+    );
+  }
+}
+
+SignUpCom.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SignUpCom);
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,7 @@ import Home from "./Home/Home";
 import NoMatch from "./NoMatch/NoMatch";
 import SignIn from "./SignIn/SignIn";
 import SignUp from "./SignUp/SignUp";
+import SignInCom from "./SignIn/SignInCom";
+import SignUpCom from "./SignUp/SignUpCom";
 
-export { Home, NoMatch, SignIn, SignUp };
+export { Home, NoMatch, SignIn, SignUp, SignInCom, SignUpCom };
