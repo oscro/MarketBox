fa0310cd33297f189cd8263b61c9b2390311f2c1
The Real One <gutierrez88@gmail.com>
2019-05-02 19:46:37 -0400

    modal

--- client/src/components/PermNav/BottomAppBar.js
+++ client/src/components/PermNav/BottomAppBar.js
@@ -6,7 +6,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Fab from '@material-ui/core/Fab';
-import AddSpaceModal from "../AddSpaceModal";
 import MenuIcon from '@material-ui/icons/Menu';
 import AddIcon from '@material-ui/icons/Add';
 import SearchIcon from '@material-ui/icons/Search';
