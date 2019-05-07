b20123d22cafb497713828c64b023a1007684355
Oscro <oscar.orchilles@gmail.com>
2019-05-07 18:50:21 -0400

    minor

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const AdSpace = require("../models/adspace");
+const AdSpace = require("../models/adSpace");
 const passport = require("../passport");
 const { ensureAuthenticated } = require('../passport/auth');
 const cloudinary = require('cloudinary');
